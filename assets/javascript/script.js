d656378c1420da6b4187fdcbff1709a233e81a8f
Rommel Jay San Jose <arjae.sj@gmail.com>
2020-06-20 15:17:06 +0800

    VC.30.10 added validate HTML function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 // when document is ready, allow functions to run
-$(document).ready(function () {
+$(document).ready(function() {
 
     function accesCodeToBeValidated() {
         // prevent default behaviour
@@ -17,7 +17,7 @@ $(document).ready(function () {
             // on click of modal
             $(noCodeModal).on("click", function() {
                 // set to hidden
-                noCodeModal.addClass("hide"); 
+                noCodeModal.addClass("hide");
             })
         }
 
@@ -32,27 +32,51 @@ $(document).ready(function () {
         if (languageSelectedByUser == "Javascript") {
             // call JSHint (created by Nitin)
             console.log("call JSHint")
-        }
-        else if (languageSelectedByUser == "HTML") {
+        } else if (languageSelectedByUser == "HTML") {
             // call the HTML API (creted by RJ)
+            validateHtml(inputtedCodeToBeValidated);
             console.log("call HTML API")
-        }
-        else if (languageSelectedByUser == "CSS") {
+        } else if (languageSelectedByUser == "CSS") {
             // call the CSS API (creted by RJ)
             console.log("call CSS API")
-        } 
-        else {
+        } else {
             // access appropriate modal div
             const noLanguageModal = $("#no-language-selected-modal")
-            // display modal
+                // display modal
             noLanguageModal.removeClass("hide")
-            // when modal is clicked on 
+                // when modal is clicked on 
             $(noLanguageModal).on("click", function() {
                 // set modal to hidden
                 noLanguageModal.addClass("hide")
             })
         }
 
+    }
+
+    // easy way to get current pages HTML
+    function validateHtml(html) {
+
+        // emulate form post
+        var formData = new FormData();
+        formData.append('out', 'json');
+        formData.append('content', html);
+
+        // make ajax call
+        $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/https://html5.validator.nu/",
+            data: formData,
+            dataType: "json",
+            type: "POST",
+            processData: false,
+            contentType: false,
+            success: function(data) {
+                console.log(data.messages); // data.messages is an array
+            },
+            error: function() {
+                console.warn(arguments);
+            }
+
+        });
     }
 
     function removePreviouslyAppendedErrors() {
@@ -66,5 +90,5 @@ $(document).ready(function () {
     // add event listener to click of clear button 
     $("#clear-page").on("click", removePreviouslyAppendedErrors)
 
-    
+
 });
