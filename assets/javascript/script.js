640427895b3a4e57746db5cb5fbdcfe513cb1f0c
Robyn H <robyn.heslop@hotmail.com>
2020-06-25 19:29:24 +0800

    updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         event.preventDefault();
 
         // create new variable containing the code to be analysed 
-        var inputtedCodeToBeValidated = $("#code-to-analyse").val();
+        const inputtedCodeToBeValidated = $("#code-to-analyse").val();
 
         // if inputted code is empty 
         if (inputtedCodeToBeValidated == "") {
@@ -27,21 +27,85 @@ $(document).ready(function() {
             return;
         }
 
+        // call highlight.JS and run inputted code through
+        const lan = hljs.highlightAuto(inputtedCodeToBeValidated);
+
+        console.log(lan)
+        // save language detected in variable
+        const codeLanguageDetected = lan.language;
+        
         // create a new variable containing the selected language from dropdown 
-        var languageSelectedByUser = $("#language-selected :selected").val();
-        selectTheLanguageAPI(languageSelectedByUser, inputtedCodeToBeValidated);
+        const languageSelectedByUser = $("#language-selected :selected").val();
+
+        console.log(languageSelectedByUser)
+
+        selectTheLanguageAPI(languageSelectedByUser, codeLanguageDetected, inputtedCodeToBeValidated)
+
     }
 
-    function selectTheLanguageAPI(languageSelectedByUser, inputtedCodeToBeValidated) {
+    function wrongLanguageModalActivation(languageSelectedByUser, inputtedCodeToBeValidated) {
+        // access appropriate error message modal 
+        const wrongLanguageModal = $("#wrong-language-selected");
+        // display modal
+        wrongLanguageModal.addClass("is-active");
+
+        $("#correct-language").on("click", function () {
+            wrongLanguageModal.removeClass("is-active");
+            console.log("clicked yes")
+            if (languageSelectedByUser == "Javascript") {
+                validateJavaScript(inputtedCodeToBeValidated);
+                renderResult();
+            }
+            else if (languageSelectedByUser == "HTML") {
+                validateHtml(inputtedCodeToBeValidated);
+            }
+            else if (languageSelectedByUser == "CSS") {
+                // call the CSS API (creted by RJ)
+                console.log("call CSS API");
+            }
+        });
+
+        $("#incorrect-language").on("click", function () {
+            wrongLanguageModal.removeClass("is-active");
+            return;
+        });
+
+        $(".modal-close").on("click", function () {
+            wrongLanguageModal.removeClass("is-active");
+            return;
+        });
+
+    }
+
+    function selectTheLanguageAPI(languageSelectedByUser, codeLanguageDetected, inputtedCodeToBeValidated) {
         if (languageSelectedByUser == "Javascript") {
-            validateJavaScript(inputtedCodeToBeValidated);
-            renderResult();
-        } else if (languageSelectedByUser == "HTML") {
-            validateHtml(inputtedCodeToBeValidated);
-        } else if (languageSelectedByUser == "CSS") {
-            validateCss(inputtedCodeToBeValidated);
-            renderResult();
-        } else {
+            if (codeLanguageDetected == "javascript") {
+                validateJavaScript(inputtedCodeToBeValidated);
+                renderResult();
+
+            } else {
+                $("#insert-language-here").text("JavaScript");
+                wrongLanguageModalActivation(languageSelectedByUser, inputtedCodeToBeValidated);
+            }
+        } 
+        else if (languageSelectedByUser == "HTML") {
+            if (codeLanguageDetected == "xml") {
+                validateHtml(inputtedCodeToBeValidated);
+                
+            } else {
+                $("#insert-language-here").text("HTML");
+                wrongLanguageModalActivation(languageSelectedByUser, inputtedCodeToBeValidated);
+            }
+        } 
+        else if (languageSelectedByUser == "CSS") {
+            if (codeLanguageDetected == "css") {
+                selectTheLanguageAPI(languageSelectedByUser, inputtedCodeToBeValidated);
+            } else {
+                $("#insert-language-here").text("CSS");
+                wrongLanguageModalActivation(languageSelectedByUser, inputtedCodeToBeValidated);
+            }
+        } 
+        else {
             // access appropriate modal div
             const noLanguageModal = $("#no-language-selected-modal");
             // display modal
@@ -53,6 +117,44 @@ $(document).ready(function() {
             });
         }
     }
+
+    function validateHtml(html) {
+        // emulate form post
+        var formData = new FormData();
+        formData.append('out', 'json');
+        formData.append('content', html);
+        // make ajax call
+        $.ajax({
+            url: "https://html5.validator.nu/",
+            data: formData,
+            dataType: "json",
+            type: "POST",
+            processData: false,
+            contentType: false,
+            success: function (result) {
+                console.log(result);
+                const errors = result.messages;
+                console.log(errors);
+                result = [];
+                if (errors && errors.length) {
+                    $.each(errors, function (index, item) {
+                        createAndPushErrorObject(item.lastLine, item.message, item.type, item.extract);
+                    });
+                    renderResult()
+                }
+            },
+        });
+    }
+
+
+
+
+    function clearLineNumbersInTextArea() {
+        const numberArea = $(".numberedtextarea-line-numbers");
+        $(numberArea).children().not(':first').remove();
+        removePreviouslyAppendedErrors()
+    }
+
 
     function removePreviouslyAppendedErrors() {
         // remove all children nodes
@@ -80,6 +182,7 @@ $(document).ready(function() {
             $.each(errors, function(index, item) {
                 createAndPushErrorObject(item.line, item.reason, item.id, item.evidence);
             });
+            renderResult();
         }
     }
 
@@ -152,6 +255,7 @@ $(document).ready(function() {
      */
     function renderResult() {
         removePreviouslyAppendedErrors();
+
         if (result && result.length) {
             const table = $("<table></table>");
             prepareErrorTableHead(table, result[0]);
@@ -169,28 +273,31 @@ $(document).ready(function() {
                 // tableDataRow.append($("<td></td>").text(item.extract));
                 table.append(tableDataRow);
             });
+
             $("#append-errors-here").append(table);
             // access modal content for number of errors found and set error count to result.lenght
             $("#total-errors-here").text(result.length);
             // access appropriate "number of errors" modal 
-            const errorTotal = $("#number-of-errors-found")
-                // display modal
+            const errorTotal = $("#number-of-errors-found");
+            // display modal
             errorTotal.addClass("is-active");
             // when modal is clicked on 
             $(errorTotal).on("click", function() {
                 // set modal to hidden
                 errorTotal.removeClass("is-active");
             });
-        } else {
+        }
+        else {
+            // activate "NO ERRORS FOUND" modal
             // access appropriate modal div
-            const noErrorsFoundModal = $("#no-errors-found")
-                // display modal
+            const noErrorsFoundModal = $("#no-errors-found");
+            // display modal
             noErrorsFoundModal.addClass("is-active");
             // when modal is clicked on 
             $(noErrorsFoundModal).on("click", function() {
                 // set modal to hidden
                 noErrorsFoundModal.removeClass("is-active");
-            })
+            });
         }
     }
 
@@ -201,18 +308,15 @@ $(document).ready(function() {
      */
     function prepareErrorTableHead(table, errorObj) {
         const tableHead = $("<tr></tr>");
-        if (errorObj.lineNo) {
-            tableHead.append($("<th class='has-text-primary'></th>").text("Line No."));
-        }
-        if (errorObj.reason) {
-            tableHead.append($("<th class='has-text-primary'></th>").text("Error Description"));
-        }
-        if (errorObj.severity) {
-            tableHead.append($("<th class='has-text-primary'></th>").text("Error Type"));
-        }
-        if (errorObj.evidence) {
-            tableHead.append($("<th class='has-text-primary'></th>").text("Code In Focus"));
-        }
+
+        tableHead.append($("<th class='has-text-primary'></th>").text("Line No."));
+        tableHead.append($("<th class='has-text-primary'></th>").text("Error Description"));
+        tableHead.append($("<th class='has-text-primary'></th>").text("Error Severity"));
+        tableHead.append($("<th class='has-text-primary'></th>").text("Code In Focus"));
+
+
+        // give error table a border
+        $(table).attr("style", "border: 4px solid #00d1b2");
         table.append(tableHead);
     }
 
@@ -220,5 +324,6 @@ $(document).ready(function() {
     $("#validate-code").on("click", accesCodeToBeValidated);
 
     // add event listener to click of clear button 
-    $("#clear-page").on("click", removePreviouslyAppendedErrors)
+    $("#clear-page").on("click", removePreviouslyAppendedErrors, clearLineNumbersInTextArea);
+
 });
