2023ab6f1b9adad184db386eda2cf1ef36d79660
Nitin Mukesh <nitinmukes@gmail.com>
2020-06-26 21:56:21 +0800

    Added comment in javascript and cleanup

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,26 @@
 //must be global variable which will store result to be rendered.
 var result = [];
 // when document is ready, allow functions to run
-$(document).ready(function() {
+$(document).ready(function () {
     // call NumberedTextArea function and add numbers to the text
     $('#code-to-analyse').numberedtextarea();
 
     // access local storage "language" and assign to variable
-    let languageValue = localStorage.getItem("language")
+    let languageValue = localStorage.getItem("language");
     // if language value is not empty, display on dropdown 
     if (languageValue !== null) {
         $("select#language-selected").val(languageValue);
     }
 
+    /**
+     * gets trigerred once user click on validate button
+     * it analyzes the selected language and text inserted by user
+     * in text editor and then triggers relevant API/error modals based on same.
+     * Also, saves the language selected by user in local storage
+     * so that on next user visit same language could be preselcted.
+     */
     function accesCodeToBeValidated() {
-        
+
         // prevent default behaviour
         event.preventDefault();
 
@@ -27,41 +34,52 @@ $(document).ready(function() {
             // display modal
             activateModal(noCodeModal);
             // on click of modal
-            $(noCodeModal).on("click", function() {
+            $(noCodeModal).on("click", function () {
                 // set to hidden
                 deactivateModal(noCodeModal);
             });
             // exit function - if no code is entered, do not continue
             return;
         }
-
-        // call highlight.JS and run inputted code through
-        const lan = hljs.highlightAuto(inputtedCodeToBeValidated);
-
-        // save language detected in variable
-        const codeLanguageDetected = lan.language;
-        
         // create a new variable containing the selected language from dropdown 
         const languageSelectedByUser = $("#language-selected :selected").val();
-
         addLatestLanguageToLocalStorage(languageSelectedByUser);
-
-        selectTheLanguageAPI(languageSelectedByUser, codeLanguageDetected, inputtedCodeToBeValidated);
+        selectTheLanguageAPI(languageSelectedByUser, inputtedCodeToBeValidated);
 
     }
 
+    /**
+     * activates current modal.
+     * @param {current modal which need to be displayed} currentModal 
+     */
     function activateModal(currentModal) {
         currentModal.addClass("is-active");
     }
 
+    /**
+     * deactivates current modal
+     * @param {current modal which need to be hidden} currentModal 
+     */
     function deactivateModal(currentModal) {
         currentModal.removeClass("is-active");
     }
 
+    /**
+     * saves current language in local storage.
+     * @param {current user selected language} languageSelectedByUser 
+     */
     function addLatestLanguageToLocalStorage(languageSelectedByUser) {
-        localStorage.setItem("language", languageSelectedByUser)
+        localStorage.setItem("language", languageSelectedByUser);
     }
 
+    /**
+     * once it is detected that user selected language is not correct
+     * then displays modal to user to confirm if language is correct
+     * and let user decide if he/she wants to change the language selection
+     * or continue with same selection and validate against same.
+     * @param {current language selected by user} languageSelectedByUser 
+     * @param {code which need to be validated} inputtedCodeToBeValidated 
+     */
     function wrongLanguageModalActivation(languageSelectedByUser, inputtedCodeToBeValidated) {
         // access appropriate error message modal 
         const wrongLanguageModal = $("#wrong-language-selected");
@@ -95,65 +113,88 @@ $(document).ready(function() {
 
     }
 
+    /**
+     * used to dynamically change modal message text about the current selected language
+     * @param {current user selected language} languageSelectedByUser 
+     */
     function updateWrongLanguageModalText(languageSelectedByUser) {
         $("#insert-language-here").text(languageSelectedByUser);
     }
 
-    function selectTheLanguageAPI(languageSelectedByUser, codeLanguageDetected, inputtedCodeToBeValidated) {
+    /**
+     * activate wrong language selection modal if detected language is different
+     * than what user has selected, otherwise triggers relevant API based on 
+     * user selected language to do validation.
+     * @param {current selected language by user} languageSelectedByUser 
+     * @param {source code which need to be validated} inputtedCodeToBeValidated 
+     */
+    function selectTheLanguageAPI(languageSelectedByUser, inputtedCodeToBeValidated) {
+        // call highlight.JS and run inputted code through
+        const lan = hljs.highlightAuto(inputtedCodeToBeValidated);
+        // store language detected in variable
+        const codeLanguageDetected = lan.language;
+        
         if (languageSelectedByUser == "Javascript") {
             if (codeLanguageDetected == "javascript") {
-                console.log("match")
                 validateJavaScript(inputtedCodeToBeValidated);
                 renderResult();
 
             } else {
-                updateWrongLanguageModalText(languageSelectedByUser)
+                updateWrongLanguageModalText(languageSelectedByUser);
                 wrongLanguageModalActivation(languageSelectedByUser, inputtedCodeToBeValidated);
             }
-        } 
+        }
         else if (languageSelectedByUser == "HTML") {
             if (codeLanguageDetected == "xml") {
                 validateHtml(inputtedCodeToBeValidated);
-                
+
             } else {
-                updateWrongLanguageModalText(languageSelectedByUser)
+                updateWrongLanguageModalText(languageSelectedByUser);
                 wrongLanguageModalActivation(languageSelectedByUser, inputtedCodeToBeValidated);
             }
-        } 
+        }
         else if (languageSelectedByUser == "CSS") {
             if (codeLanguageDetected == "css") {
                 validateCss(inputtedCodeToBeValidated);
             } else {
-                updateWrongLanguageModalText(languageSelectedByUser)
+                updateWrongLanguageModalText(languageSelectedByUser);
                 wrongLanguageModalActivation(languageSelectedByUser, inputtedCodeToBeValidated);
             }
-        } 
+        }
         else {
             // access appropriate modal div
             const noLanguageModal = $("#no-language-selected-modal");
             // display modal
             activateModal(noLanguageModal);
             // when modal is clicked on 
-            $(noLanguageModal).on("click", function() {
+            $(noLanguageModal).on("click", function () {
                 // set modal to hidden
                 deactivateModal(noLanguageModal);
             });
         }
     }
 
+    /**
+     * clears line number for text editor.
+     */
     function clearLineNumbersInTextArea() {
         const numberArea = $(".numberedtextarea-line-numbers");
         $(numberArea).children().not(':first').remove();
-        removePreviouslyAppendedErrors()
+        removePreviouslyAppendedErrors();
     }
 
+    /**
+     * clears the section where error result element will be rendered.
+     */
     function removePreviouslyAppendedErrors() {
         // remove all children nodes
         $("#append-errors-here").empty();
     }
 
     /**
-     * validates input source using JSHint API and populates result array.
+     * validates input source using JSHint API,
+     * creates error object if any error found
+     * and finally renders result to user. 
      * @param {source code which need to be validated} source 
      */
     function validateJavaScript(source) {
@@ -166,17 +207,22 @@ $(document).ready(function() {
             foo: false
         };
         JSHINT(source, options, predef);
-        console.log(JSHINT.data());
         const errors = JSHINT.data().errors;
         result = [];
         if (errors && errors.length) {
-            $.each(errors, function(index, item) {
+            $.each(errors, function (index, item) {
                 createAndPushErrorObject(item.line, item.reason, item.id, item.evidence);
             });
             renderResult();
         }
     }
 
+    /**
+     * calls W3C validate HTML API, once promise is resolved then process the response,
+     * create error objects, if any error found
+     * and finally renders result for user.
+     * @param {source code which need to be validated} html 
+     */
     function validateHtml(html) {
         // emulate form post
         var formData = new FormData();
@@ -190,12 +236,12 @@ $(document).ready(function() {
             type: "POST",
             processData: false,
             contentType: false,
-            success: function(result) {
+            success: function (response) {
                 // empty previous results
                 window.result = [];
-                const errors = result.messages;
+                const errors = response.messages;
                 if (errors && errors.length) {
-                    $.each(errors, function(index, item) {
+                    $.each(errors, function (index, item) {
                         createAndPushErrorObject(item.lastLine, item.message, item.type, item.extract);
                     });
                 }
@@ -204,32 +250,34 @@ $(document).ready(function() {
         });
     }
 
+    /**
+     * calls W3C validate CSS API to validate the source, once API responds, then process the response 
+     * and parse the text response to extract desired data, create error object if error/warning found 
+     * and finally calls renderResult function to display the result.
+     * @param {source code which need to be validated} source 
+     */
     function validateCss(source) {
         const outputFormat = "text/plain";
-        const url = `https://cors-anywhere.herokuapp.com/http://jigsaw.w3.org/css-validator/validator?text=${encodeURIComponent(source)}&warning=0&profile=css3&output=${encodeURIComponent(outputFormat)}`
+        const url = `https://cors-anywhere.herokuapp.com/http://jigsaw.w3.org/css-validator/validator?text=${encodeURIComponent(source)}&warning=0&profile=css3&output=${encodeURIComponent(outputFormat)}`;
         // make ajax call
         $.ajax({
             url,
             method: "GET"
         })
             .then(function (response) {
-                //console.log(response);
                 const lines = response.split("\n");
                 var lineNum, description, errorType, evidence;
                 result = [];
                 var errorFinished = false;
                 $.each(lines, function (index, line) {
-                    console.log(line);
                     if (line.indexOf("Warning") > -1) {
                         errorFinished = true;
                     }
                     if (line.indexOf("Line") > -1) {
-                        //console.log(line);
                         const colonSplit = line.split(":");
                         const spaceSplit = colonSplit[1].split(" ");
                         lineNum = spaceSplit[1];
                         const splitForEvidence = colonSplit[1].split(lineNum);
-                        console.log("lineNum = " + lineNum + "& evidence = " + evidence);
                         if (errorFinished) {
                             errorType = "warning";
                             description = splitForEvidence[1];
@@ -241,13 +289,12 @@ $(document).ready(function() {
                             evidence = splitForEvidence[1];
 
                         }
-                        //console.log("description = " + description);
-                        createAndPushErrorObject(lineNum, description, errorType, evidence)
+                        createAndPushErrorObject(lineNum, description, errorType, evidence);
                     }
                 });
                 renderResult();
             });
-    };
+    }
 
     /**
      * prepares error object using input fields and push it to result array.
@@ -281,18 +328,12 @@ $(document).ready(function() {
         if (result && result.length) {
             const table = $("<table></table>");
             prepareErrorTableHead(table, result[0]);
-            $.each(result, function(index, item) {
+            $.each(result, function (index, item) {
                 const tableDataRow = $("<tr></tr>");
-                // Print Javascript validator results object values
                 tableDataRow.append($("<td></td>").text(item.lineNo));
                 tableDataRow.append($("<td></td>").text(item.reason));
                 tableDataRow.append($("<td></td>").text(item.severity));
                 tableDataRow.append($("<td></td>").text(item.evidence));
-                // Print HTML validator results object values
-                // tableDataRow.append($("<td></td>").text(item.lastLine));
-                // tableDataRow.append($("<td></td>").text(item.message));
-                // tableDataRow.append($("<td></td>").text(item.type));
-                // tableDataRow.append($("<td></td>").text(item.extract));
                 table.append(tableDataRow);
             });
 
@@ -304,7 +345,7 @@ $(document).ready(function() {
             // display modal
             activateModal(errorTotal);
             // when modal is clicked on 
-            $(errorTotal).on("click", function() {
+            $(errorTotal).on("click", function () {
                 // set modal to hidden
                 deactivateModal(errorTotal);
             });
@@ -316,7 +357,7 @@ $(document).ready(function() {
             // display modal
             activateModal(noErrorsFoundModal);
             // when modal is clicked on 
-            $(noErrorsFoundModal).on("click", function() {
+            $(noErrorsFoundModal).on("click", function () {
                 // set modal to hidden
                 deactivateModal(noErrorsFoundModal);
             });
@@ -326,17 +367,13 @@ $(document).ready(function() {
     /**
      * prepares head row for error table.
      * @param {table whose head row need to be prepared} table 
-     * @param {object which need to be referred to prepare head row} errorObj 
-     */
-    function prepareErrorTableHead(table, errorObj) {
+    */
+    function prepareErrorTableHead(table) {
         const tableHead = $("<tr></tr>");
-
         tableHead.append($("<th class='has-text-primary'></th>").text("Line No."));
         tableHead.append($("<th class='has-text-primary'></th>").text("Error Description"));
         tableHead.append($("<th class='has-text-primary'></th>").text("Error Severity"));
         tableHead.append($("<th class='has-text-primary' id='focus-code-column'></th>").text("Code In Focus"));
-
-
         // give error table a border
         $(table).attr("style", "border: 4px solid #00d1b2");
         table.append(tableHead);
